6e775b0e0b29cd087de68802fcd582f22814febb
NebylytsiaKyrylo <knebylytsia.it@gmail.com>
2026-04-20 22:24:01 +0200

    Add `mart_video_top_views.sql` to calculate top 10 videos by views

---
+++ sql/marts/mart_video_top_views.sql
@@ -0,0 +1,28 @@
+DROP TABLE IF EXISTS marts.mart_video_top_views;
+CREATE TABLE marts.mart_video_top_views AS
+WITH
+    ranked_videos AS (
+        SELECT
+            dc.channel_name,
+            dv.title,
+            dv.published_at,
+            ds.video_views,
+            ds.video_likes,
+            DENSE_RANK() OVER (ORDER BY ds.video_views DESC) AS video_rank
+        FROM core.fct_video_daily_snapshot AS ds
+        INNER JOIN core.dim_video AS dv
+            ON ds.video_key = dv.video_key
+        INNER JOIN core.dim_channel AS dc
+            ON ds.channel_key = dc.channel_key
+        WHERE
+            ds.snapshot_date = CURRENT_DATE
+                     )
+SELECT
+    channel_name,
+    title,
+    published_at,
+    video_views,
+    video_likes
+FROM ranked_videos
+WHERE
+    video_rank BETWEEN 1 AND 10;
