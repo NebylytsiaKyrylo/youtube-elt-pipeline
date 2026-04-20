f1a37ce5f78a87f287ebc7ed151a274a3a69c69c
NebylytsiaKyrylo <knebylytsia.it@gmail.com>
2026-04-20 23:24:22 +0200

    Add `subscribers_count` column to `staging.yt_video_snapshot` table

--- sql/staging/ddl_staging.sql
+++ sql/staging/ddl_staging.sql
@@ -1,15 +1,10 @@
-/*
-This script creates the staging schema and table.
-*/
-
--- Create staging schema
 CREATE SCHEMA IF NOT EXISTS staging;
 
--- Create staging table
 CREATE TABLE IF NOT EXISTS staging.yt_video_snapshot (
     video_id TEXT NOT NULL,
     channel_id TEXT NOT NULL,
     channel_name TEXT,
+    subscribers_count TEXT,
     channel_start_date TEXT,
     title TEXT,
     published_at TEXT,
