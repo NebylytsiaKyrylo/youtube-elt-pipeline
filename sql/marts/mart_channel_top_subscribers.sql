c85da3f498031386260333d9e018f0161b61de9d
NebylytsiaKyrylo <knebylytsia.it@gmail.com>
2026-04-21 11:37:20 +0200

    Add `mart_channel_top_subscribers.sql` to calculate channels with the most subscribers and their metrics

---
+++ sql/marts/mart_channel_top_subscribers.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS marts.mart_channel_top_subscribers;
+CREATE TABLE marts.mart_channel_top_subscribers AS
+SELECT
+    dc.channel_name,
+    dc.subscribers_count,
+    SUM(video_views) AS total_views,
+    COUNT(ds.video_key) AS total_videos
+FROM core.fct_video_daily_snapshot AS ds
+INNER JOIN core.dim_channel AS dc
+    ON ds.channel_key = dc.channel_key
+WHERE
+    ds.snapshot_date = CURRENT_DATE
+GROUP BY
+    dc.channel_name,
+    dc.subscribers_count
+ORDER BY
+    dc.subscribers_count DESC;
