52749298b34f8be859196120cd8579eb21a9d6a5
NebylytsiaKyrylo <knebylytsia.it@gmail.com>
2026-04-21 11:27:42 +0200

    Standardize SQL formatting in `mart_video_top_views.sql` for readability and consistency

--- sql/marts/mart_video_top_views.sql
+++ sql/marts/mart_video_top_views.sql
@@ -1,22 +1,23 @@
 DROP TABLE IF EXISTS marts.mart_video_top_views;
 CREATE TABLE marts.mart_video_top_views AS
 WITH
-    ranked_videos AS (
-        SELECT
-            dc.channel_name,
-            dv.title,
-            dv.published_at,
-            ds.video_views,
-            ds.video_likes,
-            DENSE_RANK() OVER (ORDER BY ds.video_views DESC) AS video_rank
-        FROM core.fct_video_daily_snapshot AS ds
-        INNER JOIN core.dim_video AS dv
-            ON ds.video_key = dv.video_key
-        INNER JOIN core.dim_channel AS dc
-            ON ds.channel_key = dc.channel_key
-        WHERE
-            ds.snapshot_date = CURRENT_DATE
-                     )
+ranked_videos AS (
+    SELECT
+        dc.channel_name,
+        dv.title,
+        dv.published_at,
+        ds.video_views,
+        ds.video_likes,
+        DENSE_RANK() OVER (ORDER BY ds.video_views DESC) AS video_rank
+    FROM core.fct_video_daily_snapshot AS ds
+    INNER JOIN core.dim_video AS dv
+        ON ds.video_key = dv.video_key
+    INNER JOIN core.dim_channel AS dc
+        ON ds.channel_key = dc.channel_key
+    WHERE
+        ds.snapshot_date = CURRENT_DATE
+)
+
 SELECT
     channel_name,
     title,
