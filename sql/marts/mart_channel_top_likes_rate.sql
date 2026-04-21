5fc46c3e8473a9059119b618951259642521c51d
NebylytsiaKyrylo <knebylytsia.it@gmail.com>
2026-04-21 11:27:23 +0200

    Standardize SQL formatting in `mart_channel_top_likes_rate.sql` for readability and consistency

--- sql/marts/mart_channel_top_likes_rate.sql
+++ sql/marts/mart_channel_top_likes_rate.sql
@@ -1,27 +1,27 @@
 DROP TABLE IF EXISTS marts.mart_channel_top_likes_rate;
 CREATE TABLE marts.mart_channel_top_likes_rate AS
 WITH
-    aggregated_channels AS
-        (
-            SELECT
-                dc.channel_key,
-                dc.channel_name,
-                SUM(ds.video_views) AS total_views,
-                SUM(ds.video_likes) AS total_likes
-            FROM core.fct_video_daily_snapshot AS ds
-            INNER JOIN core.dim_channel AS dc
-                ON ds.channel_key = dc.channel_key
-            WHERE
-                ds.snapshot_date = CURRENT_DATE
-            GROUP BY
-                dc.channel_key,
-                dc.channel_name
-            HAVING sum(ds.video_views) >= 10000
-        )
+aggregated_channels AS (
+    SELECT
+        dc.channel_key,
+        dc.channel_name,
+        SUM(ds.video_views) AS total_views,
+        SUM(ds.video_likes) AS total_likes
+    FROM core.fct_video_daily_snapshot AS ds
+    INNER JOIN core.dim_channel AS dc
+        ON ds.channel_key = dc.channel_key
+    WHERE
+        ds.snapshot_date = CURRENT_DATE
+    GROUP BY
+        dc.channel_key,
+        dc.channel_name
+    HAVING SUM(ds.video_views) >= 10000
+)
+
 SELECT
     channel_name,
     total_views,
     total_likes,
-    round(total_likes::numeric / total_views, 3) as likes_rate
+    ROUND(total_likes::numeric / total_views, 3) AS likes_rate
 FROM aggregated_channels
 ORDER BY likes_rate DESC;
