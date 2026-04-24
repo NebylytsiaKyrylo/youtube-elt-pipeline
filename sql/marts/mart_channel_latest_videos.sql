144d7d06eef1eb5076e2ced271dd7e7d84c1a8fb
NebylytsiaKyrylo <knebylytsia.it@gmail.com>
2026-04-24 17:10:55 +0200

    Refactor `mart_channel_latest_videos` query to simplify logic using `DISTINCT ON` and improve ordering clarity.

--- sql/marts/mart_channel_latest_videos.sql
+++ sql/marts/mart_channel_latest_videos.sql
@@ -1,24 +1,13 @@
 DROP TABLE IF EXISTS marts.mart_channel_latest_videos;
 CREATE TABLE marts.mart_channel_latest_videos AS
-WITH
-latest_video_per_channel AS (
-    SELECT
-        dc.channel_name,
-        dv.title,
-        dv.published_at,
-        MAX(dv.published_at) OVER (PARTITION BY dc.channel_key) AS latest_published_at
-    FROM core.dim_video AS dv
-    INNER JOIN core.dim_channel AS dc
-        ON dv.channel_key = dc.channel_key
-)
-
-SELECT
-    channel_name,
-    title AS latest_title,
-    latest_published_at,
-    '{{ ds }}'::DATE - latest_published_at::DATE AS days_since_last_video
-FROM latest_video_per_channel
-WHERE
-    published_at = latest_published_at
+SELECT DISTINCT ON (dc.channel_key)
+    dc.channel_name,
+    dv.title AS latest_title,
+    dv.published_at AS latest_published_at,
+    '{{ ds }}'::DATE - dv.published_at::DATE AS days_since_last_video
+FROM core.dim_video AS dv
+INNER JOIN core.dim_channel AS dc
+    ON dv.channel_key = dc.channel_key
 ORDER BY
-    days_since_last_video;
+    dc.channel_key,
+    dv.published_at DESC;
