8f95bce8a3bcb6d7e6b787793808f6d68f71b615
NebylytsiaKyrylo <knebylytsia.it@gmail.com>
2026-04-28 13:50:09 +0200

    Filter inactive videos in `mart_channel_latest_videos.sql` by adding `is_active` condition.

--- sql/marts/mart_channel_latest_videos.sql
+++ sql/marts/mart_channel_latest_videos.sql
@@ -8,6 +8,7 @@ SELECT DISTINCT ON (dc.channel_key)
 FROM core.dim_video AS dv
 INNER JOIN core.dim_channel AS dc
     ON dv.channel_key = dc.channel_key
+WHERE dv.is_active = TRUE
 ORDER BY
     dc.channel_key ASC,
     dv.published_at DESC;
